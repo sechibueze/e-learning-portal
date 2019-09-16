3288a0fd7b9da13562d8d1be02872be7235e35d7
Samuel Chibueze <lightworthng@gmail.com>
2019-09-16 12:28:05 +0100

    bg-add mongoDB_URI from clever-cloud

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ mongoose.connect(dbConfig(), { useNewUrlParser: true }, (err) => {
   logger('Connected to mongoDB...');
 });
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Import 
 import userController from './controllers/userController';
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,12 +1,13 @@
 export default () => {
-
+  console.log('db URl:', process.env.MONGODB_ADDON_URI || "mongodb://localhost:27017/LIB");
+  return process.env.MONGODB_ADDON_URI || "mongodb://localhost:27017/LIB";
   // return 'mongodb+srv://lib-portal:learninblue@cluster0-jvnif.mongodb.net/test?retryWrites=true&w=majority';
-  if (process.env.NODE_ENV == 'production') {
-    return 'mongodb+srv://lib-portal:learninblue@cluster0-jvnif.mongodb.net/test?retryWrites=true&w=majority'
+  // if (process.env.NODE_ENV == 'production') {
+  //   return 'mongodb+srv://lib-portal:learninblue@cluster0-jvnif.mongodb.net/test?retryWrites=true&w=majority'
 
-  } else {
-    //dev
-    return "mongodb://localhost:27017/LIB";
-  }
+  // } else {
+  //   //dev
+  //   return process.env.MONGODB_ADDON_URI || "mongodb://localhost:27017/LIB";
+  // }
 
 }
